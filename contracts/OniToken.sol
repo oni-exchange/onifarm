4c98001f82c74cfa7bb9ec90fb2693b30f83fb11
IntegralTeam <repo@sfxdx.ru>
2021-03-15 09:23:44 +0400

    changed token name

--- contracts/OniToken.sol
+++ contracts/OniToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
 
 // OniToken with Governance.
-contract OniToken is BEP20('OniSwap Token', 'Oni') {
+contract OniToken is BEP20('Oni Token', 'ONI') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
