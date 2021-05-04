37a6973809b4d35bf70061d20ff4c9d806c2c564
IntegralTeam <repo@sfxdx.ru>
2021-05-04 14:32:57 +0300

    add dependencies & update config
    Signed-off-by: IntegralTeam <repo@sfxdx.ru>

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,41 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync('.secret').toString().trim();
+
 module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
-  //
-  compilers: {
-    solc: {
-      version: "0.6.12"
+  networks : {
+    development : {host : "127.0.0.1", port : 8545, network_id : "*"},
+//    test : {host : "127.0.0.1", port : 8545, network_id : "*"},
+//    otherhost : {host : "192.168.1.2", port : 7545, network_id : "*"},
+    bsctestnet : {
+      provider : () => new HDWalletProvider(
+          mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id : 97,
+      confirmations : 2,
+      timeoutBlocks : 200,
+      from : process.env.DEPLOYER_ACCOUNT,
+      // skipDryRun: true
+    },
+    bsc : {
+      provider : () =>
+          new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id : 56,
+      confirmations : 10,
+      timeoutBlocks : 200,
+      from : process.env.DEPLOYER_ACCOUNT,
+      // skipDryRun: true
+    },
+  },
+  plugins : [ 'truffle-plugin-verify' ],
+  api_keys : {
+    bscscan : process.env.BSCSCAN_API_KEY,
+  },
+  compilers : {
+    solc : {
+      version : "0.6.12",
+      settings :
+          {optimizer : {enabled : true, runs : 999999}, evmVersion : "istanbul"}
     }
   }
 };
